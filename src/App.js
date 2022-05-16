3af89b1b0eaca44adcaf1b8f89a53fb9d40a84c8
eman-s16 <projectm812@gmail.com>
2022-05-16 19:52:03 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './Components/Navbar';
+import Contact from './Components/Contact';
+import Index from './Components/Index';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<BrowserRouter>
+					<Navbar />
+					<Routes>
+						<Route exact path="https://eman-s16.github.io/ulta-1/" component={Index}  />
+						<Route path="https://eman-s16.github.io/ulta-1/Contact" component={Contact} />
+					</Rout
